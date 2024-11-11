7a0c05d825c27bccec817a94974bf7f3cf3da258
sribabu <sribabumandraju@gmail.com>
2024-11-11 19:15:44 +0530

    s

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -7,5 +7,5 @@ const router = express.Router();
 router.post("/register",register)
 router.post("/login",login)
 router.post("/submit-viva",createOrUpdateVivaResponse)
-router.get("/response/:studentId")
+router.get("/response/:studentId",getVivaResponses)
 export default router
